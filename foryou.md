738c74e07012bcacf82573cc873ad36f88244b66
w0oirke <96587188+w0oirke@users.noreply.github.com>
2023-11-29 20:02:50 +0900

    fsafas

--- foryou.md
+++ foryou.md
@@ -1,2 +1,12 @@
 tmdb api사용
 리액트, sass 사용
+
+디렉토리 대충
+pages : 홈페이지
+components : 많이 사용될 부분
+
+styles : 스타일 파일 저장소
+
+webpack.config.js : sass설정파일
+
+오류 있을텐데 신경 ㄴㄴ
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';  // 'useEffect' 삭제
+import React, { useState } from 'react'; 
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
